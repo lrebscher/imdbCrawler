4e3d3eb471ef0b017c743264ceff3d3954e6ce35
Lucas Rebscher <lucas.rebscher@web.de>
2017-01-25 20:47:09 +0100

    * implement unit test for query parsing with synonyms * add booleanQueryCode from previous assignment to actual class

---
+++ src/main/java/ue_inforet_bool_wordnet_study/QueryExtender.java
@@ -0,0 +1,57 @@
+package ue_inforet_bool_wordnet_study;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lrebscher on 25.01.17.
+ */
+public final class QueryExtender {
+
+    private QueryExtender() {
+    }
+
+    public static String modifiyQuery(final Map<String, Collection<String>> map, final String queryString) {
+        final String[] searchParts = queryString.split("AND|OR|NOT");
+
+        final HashSet<String> hashSet = new HashSet<>();
+
+        String modifiedQuery = queryString;
+
+        System.out.println("\n" + "modified query: " + queryString + "\n" + "original query: " + queryString);
+
+        for (final String part : searchParts) {
+            final String lowerCasePart = part.trim()
+                                             .replace("(", "")
+                                             .replace(")", "");
+            final String term = lowerCasePart.substring(lowerCasePart.indexOf(':') + 1);
+            final String key = lowerCasePart.substring(0, lowerCasePart.indexOf(':'));
+
+            if (map.containsKey(term.toLowerCase())) {
+                final List<String> synonyms = (List<String>) map.get(term.toLowerCase());
+                if (!synonyms.isEmpty()) {
+                    final StringBuilder stringBuilder = new StringBuilder("(");
+                    stringBuilder.append(key).append(":").append(term).append(" OR ");
+
+                    for (final String synonym : synonyms) {
+                        if (synonyms.indexOf(synonym) == synonyms.size() - 1) {
+                            //last element
+                            stringBuilder.append(key).append(":").append(synonym).append(")");
+                        } else {
+                            stringBuilder.append(key).append(":").append(synonym).append(" OR ");
+                        }
+                    }
+
+                    if (!hashSet.contains(key + ":" + term)) {
+                        modifiedQuery = modifiedQuery.replace(key + ":" + term, stringBuilder.toString());
+                        hashSet.add(key + ":" + term);
+                    }
+                }
+            }
+        }
+        return modifiedQuery;
+    }
+
+}
--- src/test/java/ue_inforet_bool/BooleanQueryTest.java
+++ src/test/java/ue_inforet_bool/BooleanQueryTest.java
@@ -1,8 +1,14 @@
 package ue_inforet_bool;
 
+import static ue_inforet_bool_wordnet_study.QueryExtender.modifiyQuery;
+
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 /**
  * Created by lrebscher on 03.12.16.
@@ -10,9 +16,34 @@ import org.junit.Test;
 public class BooleanQueryTest {
 
     @Test
-    public void tokenize() throws Exception {
-        Collection<String> tokens = ParseUtils.tokenize("This is a small test?");
+    public void parseQuery1() throws Exception {
+        HashMap<String, Collection<String>> map = new HashMap<>();
+        String[] synonyms = { "test", "fuck", "bae" };
+        map.put("wall", Arrays.asList(synonyms));
 
+        final String queryString = "plot:Elvis AND plot:is AND plot:wall AND type:movie";
+        final String modifiedQuery = modifiyQuery(map, queryString);
+        Assert.assertEquals("plot:Elvis AND plot:is AND (plot:wall OR plot:test OR plot:fuck OR plot:bae) AND type:movie", modifiedQuery);
     }
+
+    @Test
+    public void parseQuery2() throws Exception {
+        HashMap<String, Collection<String>> map = new HashMap<>();
+
+        String[] synonyms = { "test", "fuck", "bae" };
+        String[] synonyms1 = { "Trick", "Truck", "Trock" };
+        String[] synonyms2 = { "Weapon" };
+
+        map.put("star", Arrays.asList(synonyms));
+        map.put("trek", Arrays.asList(synonyms1));
+        map.put("wars", Arrays.asList(synonyms2));
+
+        final String queryString = "(title:Star AND title:Trek) OR (title:Star AND title:Wars) AND year:2016";
+        final String modifiedQuery = modifiyQuery(map, queryString);
+        Assert.assertEquals(
+            "((title:Star OR title:test OR title:fuck OR title:bae) AND (title:Trek OR title:Trick OR title:Truck OR title:Trock)) OR ((title:Star OR title:test OR title:fuck OR title:bae) AND (title:Wars OR title:Weapon)) AND year:2016",
+            modifiedQuery);
+    }
+
 
 }
